361106c7a988901d8be35c62f79a86488fed102a
Alpha  Habib <habibalpha19@gmail.com>
2022-03-18 16:11:25 +0200

    removed the background color

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const seconds = document.getElementById("seconds"); 
-const miliseconds = document.getElementById("msiliseconds");
-const start = document.getElementById("start"); 
-const stop = document.getElementById("stop");
-const reset = document.getElementById("reset");
+const seconds = document.getElementById("seconds"); //it has a value of "00"
+const miliseconds = document.getElementById("msiliseconds"); // it has a value of "00"
+const start = document.getElementById("start"); // start is a button
+const stop = document.getElementById("stop"); // stop is a button
+const reset = document.getElementById("reset"); //reset is a button
 
 let sec = 0;
 let milisec = 0;
 let timeInterval;
 
-const timer = () => {milisec++;
+// create a function TIMIER and set conditions
 
-
+const timer = () => {
+  milisec++;
 
   if (milisec < 9) {
     miliseconds.innerHTML = "0" + milisec;
@@ -25,29 +26,24 @@ const timer = () => {milisec++;
     milisec = 0;
     miliseconds.innerHTML = "0" + 0;
   }
-  if (sec>9){
-    seconds.innerHTML=sec; 
-  };
-}
+  if (sec > 9) {
+    seconds.innerHTML = sec;
+  }
+};
 
+// give life to our buttons 
+start.addEventListener("click", () => {
+  timeInterval = setInterval(timer,0.5);
+});
 
-start.addEventListener('click',()=>{
-  timeInterval = setInterval(timer,10);
-})
-
-stop.addEventListener('click',()=>{
+stop.addEventListener("click", () => {
   clearInterval(timeInterval);
-  
+});
 
-
-})
-
-reset.addEventListener('click',()=>{
+reset.addEventListener("click", () => {
   clearInterval(timeInterval);
-  sec=0;
-  milisec=0;
-  seconds.innerHTML=sec;
-  miliseconds.innerHTML=milisec;
-
-  
-})
+  sec = "00";
+  milisec = "00";
+  seconds.innerHTML = sec;
+  miliseconds.innerHTML = milisec;
+});
